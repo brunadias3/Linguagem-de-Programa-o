95f1bde01a204e38f60baf26e3fd7b21fd7cbd99
Bruna Dias <89141910+brunadias3@users.noreply.github.com>
2022-03-18 17:35:39 -0300

    Add files via upload

---
+++ ProjetoConta/src/projetoconta/Cliente.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetoconta;
+
+import java.util.Date;
+
+/**
+ *
+ * @author bruna
+ */
+public class Cliente {
+    String nome;
+    Date data_nascimento;
+    char sexo;
+    String rg;
+    String cpf;
+}
